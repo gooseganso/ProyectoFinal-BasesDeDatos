cc51f72a03902f063732f881a4f03915b308fa62
gooseganso <101367533+gooseganso@users.noreply.github.com>
2022-10-26 12:42:31 -0500

    iniciando

--- src/proyectofinal/basesdedatos/FXMLDocumentController.java
+++ src/proyectofinal/basesdedatos/FXMLDocumentController.java
@@ -6,10 +6,7 @@ package proyectofinal.basesdedatos;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
  *
