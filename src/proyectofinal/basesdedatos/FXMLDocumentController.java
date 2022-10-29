68c153cceee86217fd8d30be9cc88005f76cc9f7
EdJ2003 <105022466+EdJ2003@users.noreply.github.com>
2022-10-29 17:00:15 -0500

    Primera versión de la interfaz con diseño definido, con navegación entre ventanas

--- src/proyectofinal/basesdedatos/FXMLDocumentController.java
+++ src/proyectofinal/basesdedatos/FXMLDocumentController.java
@@ -10,6 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -19,6 +22,24 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private JFXButton btnLindo;
+    @FXML
+    private AnchorPane language_tab;
+    @FXML
+    private AnchorPane top_bar;
+    @FXML
+    private AnchorPane city_tab;
+    @FXML
+    private AnchorPane country_tab;
+    @FXML
+    private AnchorPane table_tab;
+    @FXML
+    private ImageView lang;
+    @FXML
+    private ImageView country;
+    @FXML
+    private ImageView city;
+    @FXML
+    private ImageView table;
     
     
     @Override
@@ -29,6 +50,49 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void doHola(ActionEvent event) {
         System.out.println("Prueba de texto");
+    }
+
+    @FXML
+    private void doTab(MouseEvent event) {
+        
+        
+        if(event.getTarget()== lang)
+        {
+            language_tab.setVisible(true);
+            country_tab.setVisible(false);
+            table_tab.setVisible(false);
+            city_tab.setVisible(false);
+        }
+        else
+        {
+          if(event.getTarget()== country)
+          {
+            language_tab.setVisible(false);
+            country_tab.setVisible(true);
+            table_tab.setVisible(false);
+            city_tab.setVisible(false);
+          }
+          else
+          {
+            if(event.getTarget()== city)
+            {
+             language_tab.setVisible(false);
+             country_tab.setVisible(false);
+             table_tab.setVisible(false);
+             city_tab.setVisible(true);
+            }
+            else
+            {
+             if(event.getTarget()== table)
+             {
+              language_tab.setVisible(false);
+             country_tab.setVisible(false);
+             table_tab.setVisible(true);
+             city_tab.setVisible(false);
+             }
+            }
+          }
+        }
     }
     
 }
--- src/proyectofinal/basesdedatos/ProyectoFinalBasesDeDato.java
+++ src/proyectofinal/basesdedatos/ProyectoFinalBasesDeDato.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
- */
+
 package proyectofinal.basesdedatos;
 
 import javafx.application.Application;
@@ -9,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,8 +18,9 @@ public class ProyectoFinalBasesDeDato extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
+        stage.initStyle(StageStyle.TRANSPARENT);
         Scene scene = new Scene(root);
-        
+        scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
