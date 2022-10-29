b8e5efc086672e065d179f957dc9fc7246f2dfb8
EdJ2003 <105022466+EdJ2003@users.noreply.github.com>
2022-10-29 18:48:11 -0500

    Tercera versión de la interfaz, con desplazamiento entres ventanas y archivos de controladores por separado

---
+++ src/controller/FXMLDocumentController.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author Natho
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private AnchorPane top_bar;
+    @FXML
+    private ImageView lang;
+    @FXML
+    private ImageView country;
+    @FXML
+    private ImageView city;
+    @FXML
+    private ImageView table;
+    @FXML
+    private AnchorPane tab;
+    @FXML
+    private AnchorPane principal_tab;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    private void doHola(ActionEvent event) {
+        System.out.println("Prueba de texto");
+    }
+
+    @FXML
+    private void doTab(MouseEvent event) throws IOException 
+    {
+        
+   
+         AnchorPane pane = FXMLLoader.load(getClass().getResource("/interfaz/Tables.fxml"));
+           
+         tab.getChildren().setAll(pane);
+             
+        
+     }
+
+    @FXML
+    private void doLang(MouseEvent event) throws IOException 
+    {
+        
+         AnchorPane pane = FXMLLoader.load(getClass().getResource("/interfaz/Language.fxml"));
+           
+         tab.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void doCountry(MouseEvent event) throws IOException
+    {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/interfaz/Country.fxml"));
+           
+         tab.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void doCity(MouseEvent event) throws IOException
+    {
+      AnchorPane pane = FXMLLoader.load(getClass().getResource("/interfaz/City.fxml"));
+           
+         tab.getChildren().setAll(pane);
+    }
+    
+}
